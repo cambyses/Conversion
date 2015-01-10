82fb2e6a8b204e72b124691626e8e1be9fc006da
cambyses <kambiz.behi@post.harvard.edu>
2015-01-10 11:41:02 -0600

    Fahrenheit to Celsius

---
+++ Fahrenheit_to_Celsius_01.py
@@ -0,0 +1,6 @@
+def main():
+       Fahrenheit = eval(input("Temperature in Fahrenheit"))
+       Celsius = (Fahrenheit - 32) * 5/9
+       print ("The temperature is", Celsius, "degree Celseius.")
+main()
+       
---
+++ Fahrenheit_to_Celsius_02.py
@@ -0,0 +1,5 @@
+def main():
+       Fahrenheit = eval(input("Temperature in Fahrenheit"))
+       print ("The temperature is", (Fahrenheit - 32) * 5/9, "degree Celseius.")
+main()
+       
