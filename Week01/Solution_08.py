efed683196238e97a6cf4ce02242606b4d81715a
cambyses <kambiz.behi@post.harvard.edu>
2015-01-12 10:00:33 -0600

    Week1

--- Week01/Solution_08.py
+++ Week01/Solution_08.py
@@ -1,6 +1,17 @@
+# F to C
+# A program for converting Fahrenheit to Celsius
+# by Kam Behi
+
 def main():
-       Fahrenheit = eval(input("Temperature in Fahrenheit"))
+       Fahrenheit = eval(input("Temperature in Fahrenheit "))
        Celsius = (Fahrenheit - 32) * 5/9
-       print ("The temperature is", Celsius, "degree Celseius.")
+       if int (Celsius) == -1:
+              print ("The temperature is ", int (Celsius), "degree Celsius.")
+       elif int (Celsius) == 0:
+              print ("The temperature is ", int (Celsius), "degree Celsius.")
+       elif int (Celsius) == 1:
+              print ("The temperature is ", int (Celsius), "degree Celsius.")
+       else:
+              print ("The temperature is ", int (Celsius), "degrees Celsius.")
 main()
        
